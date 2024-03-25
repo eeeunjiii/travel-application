61ad488acd91170a5330bbb301d37622f0094f73
eunseobb <seobseob0810@naver.com>
2024-03-25 17:08:43 +0900

    feat: repository 추가

---
+++ src/main/java/travel/travelapplication/user/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package travel.travelapplication.user.repository;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+import travel.travelapplication.user.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, ObjectId> {
+
+}
