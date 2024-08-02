30370e3f7f20f33abd4d1cf6d362876effa7c6a0
eeeunjiii <jully1010@naver.com>
2024-08-02 17:37:54 +0900

    feat: 홈 화면 수정

--- src/main/java/travel/travelapplication/user/presentation/HomeController.java
+++ src/main/java/travel/travelapplication/user/presentation/HomeController.java
@@ -1,53 +1,32 @@
 package travel.travelapplication.user.presentation;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.ResourceLoader;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import travel.travelapplication.auth.CustomOAuth2User;
-
-import java.util.Map;
+import travel.travelapplication.user.application.UserService;
+import travel.travelapplication.user.domain.User;
 
 @Controller
+@RequiredArgsConstructor
 @Slf4j
 public class HomeController {
 
-    private final ResourceLoader resourceLoader;
-
-    public HomeController(ResourceLoader resourceLoader) {
-        this.resourceLoader = resourceLoader;
-    }
-
-//    @GetMapping("/home")
-//    @ResponseBody
-//    public Resource profile() {
-//        return resourceLoader.getResource("classpath:/templates/html/home.html");
-//    }
+    private final UserService userService;
 
     @GetMapping("/home")
-    @ResponseBody
-    public Resource home(Model model, @AuthenticationPrincipal CustomOAuth2User oAuth2User) {
+    public String home(Model model, @AuthenticationPrincipal CustomOAuth2User oAuth2User) throws IllegalAccessException {
 
         if (oAuth2User == null) {
-            return resourceLoader.getResource("classpath:/templates/html/home.html");
+            return "html/home";
         }
 
-        String provider = oAuth2User.getRegistrationId();
-
-        if (provider.equals("google")) {
-            model.addAttribute("username", oAuth2User.getAttribute("name"));
-            model.addAttribute("email", oAuth2User.getAttribute("email"));
-            return resourceLoader.getResource("classpath:/templates/html/home.html");
-        } else { // naver
-            Map<Object, String> response = (Map<Object, String>) oAuth2User.getAttribute("response");
-            model.addAttribute("username", response.get("name"));
-            model.addAttribute("email", response.get("email"));
-            return resourceLoader.getResource("classpath:/templates/html/home.html");
-        }
+        User user = userService.findUserByEmail(oAuth2User);
+        model.addAttribute("user", user);
+        return "html/home";
     }
 
     @GetMapping("/loginForm")
