34dc31e472a57a915b0030ea90d08d91acb02450
eeeunjiii <jully1010@naver.com>
2024-08-02 16:28:22 +0900

    feat: 홈 화면 수정

--- src/main/java/travel/travelapplication/user/presentation/HomeController.java
+++ src/main/java/travel/travelapplication/user/presentation/HomeController.java
@@ -1,42 +1,32 @@
 package travel.travelapplication.user.presentation;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.reactive.function.client.WebClient;
 import travel.travelapplication.auth.CustomOAuth2User;
-
-import java.util.Map;
-import java.util.Optional;
+import travel.travelapplication.user.application.UserService;
+import travel.travelapplication.user.domain.User;
 
 @Controller
+@RequiredArgsConstructor
 @Slf4j
 public class HomeController {
 
-    @GetMapping("/")
-    public String home(Model model, @AuthenticationPrincipal CustomOAuth2User oAuth2User) {
+    private final UserService userService;
 
+    @GetMapping("/")
+    public String home(Model model, @AuthenticationPrincipal CustomOAuth2User oAuth2User) throws IllegalAccessException {
         if(oAuth2User==null) {
             return "home";
         }
+        User user = userService.findUserByEmail(oAuth2User);
 
-        String provider= oAuth2User.getRegistrationId();
+        model.addAttribute("user", user);
 
-        if(provider.equals("google")) {
-            model.addAttribute("username", oAuth2User.getAttribute("name"));
-            model.addAttribute("email", oAuth2User.getAttribute("email"));
-            return "home";
-        } else { // naver
-            Map<Object, String> response=(Map<Object, String>) oAuth2User.getAttribute("response");
-            model.addAttribute("username", response.get("name"));
-            model.addAttribute("email", response.get("email"));
-            return "home";
-        }
+        return "home";
     }
 
     @GetMapping("/loginForm")
